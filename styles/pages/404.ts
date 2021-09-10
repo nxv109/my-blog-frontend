cd055e3186c7e4018d62d81d4ce2db2759286a80
unknown <nxv109@gmail.com>
2021-09-10 20:37:00 +0700

    Fix mistakenly delete code

--- styles/pages/404.ts
+++ styles/pages/404.ts
@@ -5,3 +5,24 @@ export const Wrapper = styled.section`
   width: 100%;
   height: 100%;
 `;
+
+export const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Code = styled.div`
+  font-size: 8rem;
+  font-weight: ${({ theme }) => theme.fontWeights.medium};
+  margin-right: 2rem;
+  .zero {
+    color: ${({ theme }) => theme.colors.yellow};
+  }
+`;
+
+export const Text = styled.p`
+  font-size: 3rem;
+`;
