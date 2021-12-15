09c6680acbf6d63ec10c8df7d4cc5e2b73ca7352
unknown <nxv109@gmail.com>
2021-12-15 12:47:35 +0700

    add robots.txt file

--- next.config.js
+++ next.config.js
@@ -43,12 +43,4 @@ module.exports = {
 
     return config;
   },
-  async rewrites() {
-    return [
-      {
-        source: '/robots.txt',
-        destination: '/api/sitemap',
-      },
-    ];
-  },
 };
